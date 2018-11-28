ec985e6e2f38d3b537a338429fa67c408e5ced5b
Jeongkyu Shin <inureyes@gmail.com>
2018-11-29 00:34:39 +0900

    * test: add predict / serve commands for sprint test

---
+++ src/ai/backend/client/cli/predict.py
@@ -0,0 +1,31 @@
+from argparse import Namespace
+
+from . import register_command
+from .admin.sessions import sessions
+import subprocess
+from .pretty import (
+    print_info, print_wait, print_done, print_fail, print_warn,
+    format_info,
+)
+
+@register_command
+def predict(args):
+    '''
+    Predict / infer results with given data using serving deep learning models.
+    '''
+    if args.quiet:
+        vprint_info = vprint_wait = vprint_done = _noop
+    else:
+        vprint_info = print_info
+        vprint_wait = print_wait
+        vprint_done = print_done
+    proc = subprocess.run(
+        ['backend.ai run python-tensorflow:1.12-py36-serv ./serving_client.py --mount model_'+model_name+' --exec "python serving_client.py" -t '+args.sess], shell=True)
+
+
+predict.add_argument('sess',
+                 help='Specify a model serving session ID or name. ')
+predict.add_argument('--info', metavar='INFO',
+                 help='Model information of given session ID')
+predict.add_argument('-q', '--quiet', action='store_true', default=False,
+                 help='Hide execution details but show only the kernel outputs.')
---
+++ src/ai/backend/client/cli/serve.py
@@ -0,0 +1,32 @@
+from argparse import Namespace
+
+from . import register_command
+from .admin.sessions import sessions
+from .pretty import (
+    print_info, print_wait, print_done, print_fail, print_warn,
+    format_info,
+)
+
+import subprocess
+
+@register_command
+def serve(args):
+    '''
+    Serves deep learning models on the fly.
+    Currently supports Servable models for TensorFlow Serving.
+    '''
+    if args.quiet:
+        vprint_info = vprint_wait = vprint_done = _noop
+    else:
+        vprint_info = print_info
+        vprint_wait = print_wait
+        vprint_done = print_done
+    vprint_info("Loading model")
+    model_name = args.model
+    proc = subprocess.run(
+        ['backend.ai run python-tensorflow:1.12-py36-serv ./resnet_client.py --mount model_'+model_name+' --exec "ls -al;ps -ua;python ./model_resnet_v2/resnet_client.py"'], shell=True)
+
+serve.add_argument('model',
+                 help='Serving model name. ')
+serve.add_argument('-q', '--quiet', action='store_true', default=False,
+                 help='Hide execution details but show only the kernel outputs.')
