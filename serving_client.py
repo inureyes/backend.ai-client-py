c0396b28694cb5f7ab4ed84e29235382a768fd4f
Jeongkyu Shin <inureyes@gmail.com>
2018-11-29 00:33:53 +0900

    * test: serving client python script for demo play

---
+++ serving_client.py
@@ -0,0 +1,22 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(description='Predict using specific model')
+parser.add_argument('model', metavar='MODEL', type=str,
+                   help='Model name to use.')
+parser.add_argument('payload', metavar='PAYLOAD', type=str,
+                   help='JSON-type payload to request prediction to model.')
+
+args = parser.parse_args()
+
+SERVER_URL = 'http://localhost:8501/model_'+args.model+':predict'
+
+def main():
+  predict_request = args.payload
+  response = requests.post(SERVER_URL, data=predict_request)
+  response.raise_for_status()
+  prediction = response.json()['predictions']
+  print(prediction)
+
+if __name__ == '__main__':
+  main()
